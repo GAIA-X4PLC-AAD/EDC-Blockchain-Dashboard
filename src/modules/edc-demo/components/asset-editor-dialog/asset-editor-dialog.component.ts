2d816e2c48a25037f6b783200433efd01d3c4767
schuberr <robert.schubert@msg.group>
2024-09-27 11:05:43 +0200

    enabled AzureStorage in Dashboard

--- src/modules/edc-demo/components/asset-editor-dialog/asset-editor-dialog.component.ts
+++ src/modules/edc-demo/components/asset-editor-dialog/asset-editor-dialog.component.ts
@@ -22,8 +22,9 @@ export class AssetEditorDialog implements OnInit {
   contenttype: string = '';
 
   storageTypeId: string = 'HttpData';
-  account: string = '';
+  account: string = 'msgedcstorage';
   container: string = 'src-container';
+  blobname: string = '';
 
   originator: string = 'http://connector-1:8184/protocol';
   baseUrl: string = "http://techslides.com/demos/samples/sample.txt";
@@ -446,6 +447,10 @@ export class AssetEditorDialog implements OnInit {
         "type": this.storageTypeId,
         "name": this.id,
         "baseUrl": this.baseUrl,
+        "account": this.account,
+        "container": this.container,
+        "blobname": this.blobname,
+        "keyName": `${this.account}-key1`
       }
     };
     this.dialogRef.close({assetInput});
