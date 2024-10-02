98b7b32109d78caaf13a5264fd004098227fa9e3
schuberr <robert.schubert@msg.group>
2024-10-02 15:50:13 +0200

    added missing MatAutocompleteModule imports

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -6,6 +6,7 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {LayoutModule} from '@angular/cdk/layout';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
--- src/modules/edc-demo/edc-demo.module.ts
+++ src/modules/edc-demo/edc-demo.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
